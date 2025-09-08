b7c47ea09bce6fc1c4726ba853ae65f9c5ebaecc
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2025-09-08 23:33:35 +1000

    feat(css-the-complete-guide-2025 section16): finished until Assignment 9: Time to Practice - CSS Transitions

---
+++ css/udemy-css-the-complete-guide-2025/projects/16-understanding-and-applying-transitions-project/shared.js
@@ -0,0 +1,53 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+// console.dir(backdrop.style["background-image"]);
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    // backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // modal.className = "open"; // This will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function () {
+      backdrop.classList.add("open");
+    }, 10);
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  // mobileNav.style.display = "none";
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.display = "none";
+  setTimeout(function () {
+    backdrop.classList.remove("open");
+  }, 200);
+}
+
+toggleButton.addEventListener("click", function () {
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  mobileNav.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function () {
+    backdrop.classList.add("open");
+  }, 10);
+});
