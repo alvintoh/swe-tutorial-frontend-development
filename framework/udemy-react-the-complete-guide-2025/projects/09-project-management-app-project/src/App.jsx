e5344283aa3e5b86ae79621e46df2efc351dd547
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2025-11-22 23:27:10 +1100

    feat(react-the-complete-guide-2025 section9): finished until 150. Styling the Sidebar & Button with Tailwind CSS

---
+++ framework/udemy-react-the-complete-guide-2025/projects/09-project-management-app-project/src/App.jsx
@@ -0,0 +1,13 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
+function App() {
+  return (
+    <>
+      <main className="h-screen my-8">
+        <ProjectsSidebar />
+      </main>
+    </>
+  );
+}
+
+export default App;
