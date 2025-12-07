6ee8792f26f5eea2609e5be8389de117935cefda
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2025-12-07 13:13:02 +1100

    Revert "feat(react-the-complete-guide-2025 section12): finished until 202. Splitting Components Up To Solve Problems"
    This reverts commit 2ca9a31b9b4909cd65e230ad6b198c54b1e64c7e.

--- framework/udemy-react-the-complete-guide-2025/projects/12-building-a-quiz-app-project/src/components/Quiz.jsx
+++ framework/udemy-react-the-complete-guide-2025/projects/12-building-a-quiz-app-project/src/components/Quiz.jsx
@@ -2,63 +2,61 @@ import { useCallback, useState } from "react";
 
 import QUESTIONS from "../../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
-import Questions from "./Question.jsx";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
-  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
-
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      setAnswerState("answered");
-      setUserAnswers((prevUserAnswers) => {
-        return [...prevUserAnswers, selectedAnswer];
-      });
-
-      setTimeout(() => {
-        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
-
-        setTimeout(() => {
-          setAnswerState("");
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
 
   const handleSkipAnswer = useCallback(() => {
     handleSelectAnswer(null);
   }, [handleSelectAnswer]);
 
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
   if (quizIsComplete) {
     return (
       <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy icon" />
+        <img src={quizCompleteImg} alt="Quiz Complete" />
         <h2>Quiz Completed!</h2>
       </div>
     );
   }
 
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(
+    () => Math.random() - 0.5
+  );
+
   return (
     <div id="quiz">
-      <Questions
-        key={activeQuestionIndex}
-        questionNumber={activeQuestionIndex + 1}
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
-        onSelectAnswer={handleSelectAnswer}
-        onSkipAnswer={handleSkipAnswer}
-      />
+      <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
+        <h2>
+          Q{activeQuestionIndex + 1}: {QUESTIONS[activeQuestionIndex].text}
+        </h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer, index) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                Ans:{index + 1} {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
