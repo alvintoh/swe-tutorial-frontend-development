461842cac0ad158cf0446f8ca391e3260061b781
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2025-12-17 21:50:36 +1100

    feat(react-the-complete-guide-2025 section14): finished until 229. Module Resources

--- framework/udemy-react-the-complete-guide-2025/projects/14-class-based-components-project/src/components/UserFinder.js
+++ framework/udemy-react-the-complete-guide-2025/projects/14-class-based-components-project/src/components/UserFinder.js
@@ -1,32 +1,30 @@
 import { Component, Fragment } from "react";
 
+import UsersContext from "../store/users-context";
+import ErrorBoundary from "./ErrorBoundary";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
 
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
-
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
   }
 
   componentDidMount() {
     // Send http request...
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
@@ -43,7 +41,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
