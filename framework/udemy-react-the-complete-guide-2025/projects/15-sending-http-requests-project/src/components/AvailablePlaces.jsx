315059e556ec2b74e410bf9a2a68e3ac09ecf19c
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2025-12-20 23:33:00 +1100

    feat(react-the-complete-guide-2025 section15): finished until 242. Sending Data with POST Requests

--- framework/udemy-react-the-complete-guide-2025/projects/15-sending-http-requests-project/src/components/AvailablePlaces.jsx
+++ framework/udemy-react-the-complete-guide-2025/projects/15-sending-http-requests-project/src/components/AvailablePlaces.jsx
@@ -1,24 +1,58 @@
 import { useEffect, useState } from "react";
 
+import { fetchAvailablePlaces } from "../http.js";
+import { sortPlacesByDistance } from "../loc.js";
+import Error from "./Error.jsx";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+      }
     }
 
     fetchPlaces();
   }, []);
 
+  if (error) {
+    return (
+      <Error
+        title="An error occurred!"
+        message={error.message}
+        onConfirm={() => setError(null)}
+      />
+    );
+  }
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
