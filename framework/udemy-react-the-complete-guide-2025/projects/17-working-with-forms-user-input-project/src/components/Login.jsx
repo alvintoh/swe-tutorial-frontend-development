cfaa1609fbf1e6e76556cf9a3745681ff4aa9937
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2025-12-26 00:01:54 +1100

    feat(react-the-complete-guide-2025 section17): finished until 264. Mixing Custom & Built-in Validation Props

--- framework/udemy-react-the-complete-guide-2025/projects/17-working-with-forms-user-input-project/src/components/Login.jsx
+++ framework/udemy-react-the-complete-guide-2025/projects/17-working-with-forms-user-input-project/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -10,7 +12,14 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
+    const emailIsInvalid = enteredEmail.includes("@");
+
+    if (!emailIsInvalid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    console.log("Sending HTTP request...");
   }
 
   function handleInputChange(identifier, value) {
@@ -30,6 +39,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && "Please enter a valid email address."}
+          </div>
         </div>
 
         <div className="control no-margin">
