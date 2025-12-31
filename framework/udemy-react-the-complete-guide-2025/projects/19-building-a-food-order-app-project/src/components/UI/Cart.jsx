87e72a8e76f2f8842fc1abfce9d0c623e73065f5
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2026-01-01 01:07:44 +1100

    feat(react-the-complete-guide-2025 section19): finished until 293. Opening the Cart in the Modal via a New Context

---
+++ framework/udemy-react-the-complete-guide-2025/projects/19-building-a-food-order-app-project/src/components/UI/Cart.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+
+import CartContext from "../../store/CartContext";
+import UserProgressContext from "../../store/UserProgressContext";
+import { currencyFormatter } from "../../util/formatting";
+import Button from "./Button";
+import Modal from "./Modal";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
+    return totalPrice + item.price * item.quantity;
+  }, 0);
+
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={userProgressCtx.showCheckout}>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
