41197850686fcf7f1c49170b9c0da82b7c05aab4
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2026-02-02 22:59:56 +1100

    feat(react-the-complete-guide-2025 section27): finished until 518. Theory Wrap Up

---
+++ framework/udemy-react-the-complete-guide-2025/projects/27-react-server-components-project/app/page.js
@@ -0,0 +1,25 @@
+import ErrorBoundary from "@/components/ErrorBoundary";
+import UsePromiseDemo from "@/components/UsePromisesDemo";
+import fs from "node:fs/promises";
+import { Suspense } from "react";
+
+export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve, reject) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      const users = JSON.parse(data);
+      // resolve(users);
+      reject(new Error("Error!"));
+    }, 2000),
+  );
+
+  return (
+    <main>
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
+    </main>
+  );
+}
