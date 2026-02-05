a0052bab8f262430900525b6f5cbfc44a68d747b
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2026-02-05 23:36:53 +1100

    feat(react-the-complete-guide-2025 section30): finished until 566. Module Resources

---
+++ framework/udemy-react-the-complete-guide-2025/projects/30-replacing-redux-with-react-hooks-project/src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import configureProductsStore from "./hooks-store/products-store";
+import "./index.css";
+
+configureProductsStore();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
