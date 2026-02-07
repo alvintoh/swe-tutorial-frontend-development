3093d8bed84bfbc7182c47a48724c9f3a4e081f7
ALVIN-DESKTOP\Alvin Toh <alvin4292@gmail.com>
2026-02-08 00:28:18 +1100

    feat(react-the-complete-guide-2025 section32): finished until 600. Add Styling

---
+++ framework/udemy-react-the-complete-guide-2025/projects/32-intro-typescript-project/ts-basics/no-typescript.js
@@ -0,0 +1,7 @@
+function addGeneral(a, b) {
+  return a + b;
+}
+
+const generalResult = add("2", "5");
+
+console.log(generalResult);
---
+++ framework/udemy-react-the-complete-guide-2025/projects/32-intro-typescript-project/ts-basics/with-typescript.ts
@@ -0,0 +1,7 @@
+function tsAdd(a: number, b: number): number {
+  return a + b;
+}
+
+const tsResult = tsAdd(2, 5);
+
+console.log(tsResult);
